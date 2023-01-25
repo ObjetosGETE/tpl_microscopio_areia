2efccd694536f9149510cd057c6762998d7b0f61
jpcoelhosenac <jpcoelho@senacrs.com.br>
2023-01-25 17:37:52 -0300

    Correções de estrutura, de javascript e estilos. Agora o site está adaptativo a qualquer tela, modais responsívas e não quebra mais. Adicionado js que reduz o objeto inteiro 'escalarmente' e css que mantem o conteúdo sempre alinhado ao centro.

--- js/js_microscopio_areia.js
+++ js/js_microscopio_areia.js
@@ -1,32 +1,59 @@
 $(".botao1").on("click", function () {
     $(this).prop("disabled", true);
-    $("#seta2").removeClass("some");
-    $("#seta1").addClass("some");
-    $("#areia2").removeClass("some");
-    $("#areia1").addClass("some");
+    $("#seta2").removeClass("d-none");
+    $("#seta1").addClass("d-none");
+    $("#areia2").removeClass("d-none");
+    $("#areia1").addClass("d-none");
     $(".botao2").prop("disabled", false);
 });
 
 $(".botao2").on("click", function () {
     $(this).prop("disabled", true);
-    $("#seta2").addClass("some");
-    $("#seta1").removeClass("some");
-    $("#areia2").addClass("some");
-    $("#areia1").removeClass("some");
+    $("#seta2").addClass("d-none");
+    $("#seta1").removeClass("d-none");
+    $("#areia2").addClass("d-none");
+    $("#areia1").removeClass("d-none");
     $(".botao1").prop("disabled", false);
 });
 
 $("#iniciar").on("click", function () {
-    $("#microscopio").removeClass("oculto");
-    $("#capaMicroscopio").addClass("oculto");
+    $("#microscopio").removeClass("d-none");
+    $("#capaMicroscopio").addClass("d-none");
 });
 
-$(window).bind('resize', function () {
-    if (screen.height > screen.width) {
-        $('.body-microscopio').addClass('horizontal');
-    } else {
-        $('.body-microscopio').removeClass('horizontal');
-        $('.modal-dialog').removeClass('modal-fullscreen');
-        $('.modal-dialog').addClass('modal-lg');
+// inicia o resize
+resizeBodyMicroscopio()
+        
+function resizeBodyMicroscopio(){
+    var largura = 1920;
+    var altura = 1080;
+
+    var larguraScreen = $(window).width();
+    var alturaScreen = $(window).height();
+    var proporcao;
+    if (altura < largura){
+        proporcao = ((alturaScreen * 100) / altura)/100;
+    }else{
+       proporcao = ((larguraScreen * 100) / largura)/100;
     }
-});
+    
+
+    $(".body-microscopio").css("transform", "scale("+ proporcao +")")
+}
+
+// Ajusta o resize caso o usuário mecha na janela.
+$(window).resize(function (){
+    resizeBodyMicroscopio()
+})
+
+// $(window).bind('resize', function () {
+//     if (screen.height > screen.width) {
+//         $('.body-microscopio').addClass('horizontal');
+//     } else {
+//         $('.body-microscopio').removeClass('horizontal');
+//         $('.modal-dialog').removeClass('modal-fullscreen');
+//         $('.modal-dialog').addClass('modal-lg');
+//     }
+// });
+
+
